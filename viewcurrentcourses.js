6cfa1ba1a2e557e9223534fdf6fc433cbdcf3c55
sessoh1 <sessoh1@live.maryville.edu>
2024-08-11 00:13:46 +0300

    View Current Courses
    Sami

---
+++ viewcurrentcourses.js
@@ -0,0 +1,23 @@
+document.getElementById('my-education').addEventListener('click', function() {
+    window.open('about:blank', '_blank');
+});
+
+document.getElementById('documents').addEventListener('click', function() {
+    window.open('about:blank', '_blank');
+});
+
+document.getElementById('reports').addEventListener('click', function() {
+    window.open('about:blank', '_blank');
+});
+
+document.getElementById('my-account').addEventListener('click', function() {
+    window.open('about:blank', '_blank');
+});
+
+document.getElementById('completed-courses').addEventListener('click', function() {
+    window.open('about:blank', '_blank');
+});
+
+document.getElementById('add-new-course').addEventListener('click', function() {
+    window.open('about:blank', '_blank');
+});
